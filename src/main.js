264caad72a75e75118d06e4e891dfbebe187d716
Franny Medico <m.francisca.medico@gmail.com>
2022-02-06 13:51:06 +0100

    created .env for token and installed mapbox dependencies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 createApp(App).use(store).use(router).mount("#app");
