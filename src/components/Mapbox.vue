1b97fb1e87f5878e58ea447fcd48a914455ad8d9
Franny Medico <m.francisca.medico@gmail.com>
2022-02-06 13:52:03 +0100

    created mapbox component and added to home view

---
+++ src/components/Mapbox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="map">
+    <p>HOLA IRENE</p>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import mapboxgl from "mapbox-gl";
+
+export default defineComponent({
+  name: "Map",
+  components: {},
+  data() {
+    return {
+      access_token: process.env.VUE_APP_ACCESS_TOKEN,
+    };
+  },
+
+  methods: {
+    async getMap() {
+      mapboxgl.accessToken = this.access_token;
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+      map.on("load", () => {});
+    },
+  },
+
+  mounted() {
+    this.getMap();
+  },
+});
+</script>
+
+<style scoped>
+#map {
+  height: 500px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Welcome to Ciclica Map</h1>
+    <h2>Explore the map, or create your own marks</h2>
+    <div class="map">
+      <Mapbox />
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+import { defineComponent } from "vue";
+import Mapbox from "@/components/Mapbox.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Mapbox,
+  },
+});
+</script>
